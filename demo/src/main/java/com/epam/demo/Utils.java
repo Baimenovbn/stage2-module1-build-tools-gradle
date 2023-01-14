cdb4e6046ace20fffbe6a8afcefcb29954c81bca
Baimenovbn <baibn@mail.ru>
2023-01-14 17:24:07 +0600

    fix: number format exception for list

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().mapToDouble(Double::parseDouble).allMatch(num -> num > 0);
+        return args.stream().mapToDouble(num -> {
+            try {
+                return Double.parseDouble(num);
+            } catch (NumberFormatException e) {
+                return -1;
+            }
+        }).allMatch(num -> num > 0);
     }
 }
